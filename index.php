fb27ab34c6d6aea99bd750859278e710ea33dcf9
KidFlashReverse <103575827+KidFlashReverse@users.noreply.github.com>
2023-06-09 02:14:08 -0300

    Videos Recomendados

--- index.php
+++ index.php
@@ -110,6 +110,8 @@ if(isset($_SESSION['logado']) == false){
                 </div>
 
             <div class="videos">
+                <h1>Videos Recomendados</h1>
+                <hr>
                 <div id="carouselExampleIndicators" class="carousel slide">
                     <div class="carousel-indicators" style="position: relative; top: 420px;">
                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
